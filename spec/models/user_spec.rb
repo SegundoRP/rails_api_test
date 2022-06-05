c3f8fb814ce1365d46406eb7b49b8219fdee4722
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-06-05 18:32:17 -0500

    add tests user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'validate presence of required fields'do
+      should validate_presence_of(:email)
+      should validate_presence_of(:name)
+      should validate_presence_of(:auth_token)
+      should validate_presence_of(:user_id)
+    end
+
+    it 'Validate relations' do
+      should have_many(:posts)
+    end
+  end
 end
