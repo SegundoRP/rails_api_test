dc6557525d761b8ea063d2a2c3119c394196ec50
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-06-08 05:33:13 -0500

    create factory bots

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+
+  describe 'GET/post' do
+    before { get '/post' }
+
+    it 'should return OK' do
+      payload = JSON.parse(response.body)
+      expect(payload).to be_empty
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
+  describe 'with data in the DB' do
+    before { get '/post' }
+    let(:posts) { create_list(:post, 10, published: true) }  # el let esta creando la variable post esto es de rpsec, lo de {} es factory bot
+
+    it 'should return all the published posts' do
+      payload = JSON.parse(response.body)
+      expect(payload.size).to eq(posts.size)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /post/{id}' do
+    let(:posts) { create(:post) }
+
+    it 'should return all the published posts' do
+      get "/post/#{post.id}"
+      payload = JSON.parse(response.body)
+      expect(payload.size).to_not be_empty
+      expect(payload['id']).to eq(post.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
