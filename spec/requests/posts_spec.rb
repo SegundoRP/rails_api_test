55018f9e20300748a2257ffc152ef7053400f84a
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-06-08 22:48:04 -0500

    pass tests index and show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
+require "byebug"
 
-RSpec.describe 'Posts', type: :request do
+RSpec.describe "Posts", type: :request do
 
-  describe 'GET/post' do
-    before { get '/post' }
+  describe "GET /posts" do
+    before { get '/posts' }
 
-    it 'should return OK' do
+    it "should return OK" do
       payload = JSON.parse(response.body)
       expect(payload).to be_empty
       expect(response).to have_http_status(200)
     end
-
   end
 
-  describe 'with data in the DB' do
-    before { get '/post' }
-    let(:posts) { create_list(:post, 10, published: true) }  # el let esta creando la variable post esto es de rpsec, lo de {} es factory bot
+  describe "with data in the DB" do
+    let!(:posts) { create_list(:post, 10, published: true) }
 
-    it 'should return all the published posts' do
+    it "should return all the published posts" do
+      get '/posts'
       payload = JSON.parse(response.body)
       expect(payload.size).to eq(posts.size)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe 'GET /post/{id}' do
-    let(:post) { create(:post) }
+  describe "GET /post/{id}" do
+    let!(:post) { create(:post) }
 
-    it 'should return a post' do
-      get "/post/#{post.id}"
+    it "should return a post" do
+      get "/posts/#{post.id}"
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
-      expect(payload['id']).to eq(post.id)
+      expect(payload["id"]).to eq(post.id)
       expect(response).to have_http_status(200)
     end
   end
