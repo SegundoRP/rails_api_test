324922664c3c00f7a968abd63eb027f5855f7402
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-06 00:07:53 -0500

    test without auth in posts

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Posts with authentication", type: :request do
   let!(:auth_headers) { { 'Authorization' => "Bearer #{user.auth_token}" } }
   let!(:other_auth_headers) { { 'Authorization' => "Bearer #{other_user.auth_token}" } }
   # Todo esto son datos de prueba que se necesita y se podran utilizar en las pruebas
-  let!(:create_params) { { "title" => "title", "content" => "content", "published" => true }}
+  let!(:create_params) { { 'post' => { "title" => "title", "content" => "content", "published" => true }}}
 
   describe "GET /posts/{id}" do
     context 'with valid auth' do
@@ -57,22 +57,32 @@ RSpec.describe "Posts with authentication", type: :request do
   describe "POST /posts" do
     # con auth -> crear
     context 'with valid auth' do
-      before { post "/posts/#{other_user_post_draft.id}", headers: auth_headers }
-      # antes se hace la peticion del post
+      before { post "/posts/", params: create_params, headers: auth_headers }
 
       context 'payload' do
         subject { payload }
-        # ese es el sujeto de pruebas
+        it { is_expected.to include(:id, :title, :content, :published, :author) }
+      end
+
+      context 'response' do
+        subject { response }
+        it { is_expected.to have_http_status(:created) }
+      end
+    end
+    #  sin auth -> !crear -> 401
+    context 'without auth' do
+      before { post "/posts", params: create_params }
+
+      context 'payload' do
+        subject { payload }
         it { is_expected.to include(:error) }
       end
 
       context 'response' do
         subject { response }
-        it { is_expected.to have_http_status(:not_found) }
+        it { is_expected.to have_http_status(:unauthorized) }
       end
-
     end
-    #  sin auth -> !crear -> 401
   end
 
   describe "PUT /posts" do
