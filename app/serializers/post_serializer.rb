b94f7ac368fd0fbcf213d6ff4cb339c462eb8146
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-07 05:24:51 -0500

    update serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,11 +4,12 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :published, :author # como auhtor no pertenece al modelo se debe definir
 
   def author
-    user = self.object.user
+    user = self.object.user #self.object esta haciendo referencia al post que esta siendo serializado
     {
       name: user.name,
       email: user.email,
       id: user.id
     }
+    # Asi estamos definiedo como queremos que se vea el campo author en la representacion json de post
   end
 end
