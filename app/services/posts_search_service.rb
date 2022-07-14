c1dd711ba978a31cf12514bff1278fbc082b2063
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-14 00:08:30 -0500

    enable caching with rails dev:cache

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -11,3 +11,6 @@ end
 # bases dedatos sql no estan optimizadas para hacer busquedas en texto, pueden llegar a ser muy lentas
 # para acelerar a busqueda usaremos caching
 # primero se busca por texto luego se asigna los ids y apartir de segunda bsuqeda sera mas rapido ya que busca con ids
+
+# por defecto el caching no esta activado usar en consola " rails dev:cache" para activarlo
+# y en config/environments/development.rb  config.action_controller.perform_caching = true
