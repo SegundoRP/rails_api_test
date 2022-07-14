2936139f43bcb253bf5d8504ab284e71f4e23301
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-13 23:55:28 -0500

    add search with caching

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,13 @@
 class PostsSearchService
   def self.search(curr_posts, query)
-    curr_posts.where("title like '%#{query}%'")
+    posts_id = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+      curr_posts.where("title like '%#{query}%'").map(&:id)
+    end
+
+    curr_posts.where(id: posts_id)
   end
 end
+
+# bases dedatos sql no estan optimizadas para hacer busquedas en texto, pueden llegar a ser muy lentas
+# para acelerar a busqueda usaremos caching
+# primero se busca por texto luego se asigna los ids y apartir de segunda bsuqeda sera mas rapido ya que busca con ids
